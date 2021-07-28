ea8e7bfae2947dd1eb0928509da631a486512824
betterchalk-olayeni <olayeni@betterchalk.com>
2021-07-28 08:59:57 +0000

    Learning TypeScript-Loopback

---
+++ app.js
@@ -0,0 +1,8 @@
+var HelloWorld = /** @class */ (function () {
+    function HelloWorld(message) {
+        console.log(message);
+    }
+    return HelloWorld;
+}());
+var hello = new HelloWorld("Olayeni- Welcome to TS");
+console.log(hello);
---
+++ app.ts
@@ -0,0 +1,9 @@
+class HelloWorld {
+    constructor(message){
+        console.log(message);
+        
+    }
+}
+
+const hello = new HelloWorld("Olayeni- Welcome to TS");
+console.log(hello);
