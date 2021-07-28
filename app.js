5474a0c5c66988516ae523e02e52ed505b4d9196
betterchalk-olayeni <olayeni@betterchalk.com>
2021-07-28 09:26:36 +0000

    Learning TypeScript-Loopback

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Lesson 3
 var HelloWorld = /** @class */ (function () {
     function HelloWorld(message) {
         console.log(message);
@@ -6,3 +7,49 @@ var HelloWorld = /** @class */ (function () {
 }());
 var hello = new HelloWorld("Olayeni- Welcome to TS");
 console.log(hello);
+//Lesson 4
+function LoggerAndReturn(thing) {
+    return thing;
+}
+var message = LoggerAndReturn("HELLO WOLDR");
+console.log(message);
+var messageB = LoggerAndReturn(2);
+console.log(messageB);
+//Lesson 5
+function getArray(items) {
+    return new Array().concat(items);
+}
+var myNumArr = getArray([100, 200, 300]);
+var myStrArr = getArray(["Hello", "Olayeni", "Anifowose"]);
+//TypeScript can also infer the type of the arguments being passed into the array
+var myNumArrInf = getArray([1, 2, 3, 4, 5]);
+var myStrArrInf = getArray(["I'm", "learning", "TS"]);
+console.log(myNumArr);
+console.log(myStrArr);
+console.log(myNumArrInf);
+console.log(myStrArrInf);
+//Lesson 6
+//multiple type variables
+function getInfo(id, name) {
+    console.log(typeof id + ',' + typeof name);
+}
+getInfo(34, "Yensss");
+//generic with non generic type
+function displayType(id, name) {
+    console.log(typeof id + ',' + typeof name);
+}
+displayType(32, "STRING!");
+//Lesson 7 
+//Generic Constraints
+var Customer = /** @class */ (function () {
+    function Customer(fname, lname) {
+        this.firstname = fname;
+        this.lastname = lname;
+    }
+    return Customer;
+}());
+function customerLogger(customer) {
+    console.log(customer.firstname + " " + customer.lastname);
+}
+var customer = new Customer("Mobolaji", "Anifowose");
+customerLogger(customer);
--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+//Lesson 3
 class HelloWorld {
     constructor(message){
         console.log(message);
@@ -7,3 +8,70 @@ class HelloWorld {
 
 const hello = new HelloWorld("Olayeni- Welcome to TS");
 console.log(hello);
+
+//Lesson 4
+function LoggerAndReturn<T>(thing:T): T{
+    return thing;
+}
+
+const message:string = LoggerAndReturn<string>("HELLO WOLDR")
+console.log(message);
+
+const messageB: number = LoggerAndReturn(2);
+console.log(messageB);
+
+//Lesson 5
+function getArray<T>(items:T[]): T[]{
+    return new Array<T>().concat(items);
+}
+
+let myNumArr = getArray<number>([100,200,300]);
+let myStrArr = getArray<string>(["Hello", "Olayeni", "Anifowose"])
+
+//TypeScript can also infer the type of the arguments being passed into the array
+let myNumArrInf = getArray([1,2,3,4,5]);
+let myStrArrInf = getArray(["I'm", "learning", "TS"])
+
+console.log(myNumArr);
+console.log(myStrArr);
+
+console.log(myNumArrInf);
+console.log(myStrArrInf);
+
+
+//Lesson 6
+//multiple type variables
+function getInfo<T,U>(id:T, name:U):void{
+    console.log(typeof id + ',' + typeof name)
+}
+
+getInfo<number, string>(34, "Yensss")
+
+//generic with non generic type
+function displayType<T>(id:T, name:string):void{
+    console.log(typeof id + ',' + typeof name);
+}
+
+displayType<number>(32, "STRING!")
+
+
+//Lesson 7 
+//Generic Constraints
+class Customer{
+    firstname:string;
+    lastname:string;
+
+    constructor(fname:string, lname: string){
+        this.firstname = fname;
+        this.lastname = lname;
+    }
+
+}
+
+function customerLogger<T extends Customer>(customer:T):void{
+    console.log(`${customer.firstname} ${customer.lastname}`);
+}
+
+let customer = new Customer("Mobolaji", "Anifowose");
+customerLogger(customer);
+
